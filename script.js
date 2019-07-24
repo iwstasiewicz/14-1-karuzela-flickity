415c2a7ff16b9bdbe3a4b154f807513ea9855924
Iwona Stasiewicz <iiwka@o2.pl>
2019-07-24 16:39:18 +0200

    add finished project

---
+++ script.js
@@ -0,0 +1,24 @@
+var flkty = new Flickity('.carousel');
+
+// FUNKCJA DO BUTTONA "RESTART"
+
+// var previousButton = document.querySelector('.button--previous');
+// previousButton.addEventListener('click', function () {
+//   flkty.previous();
+// });
+
+// var previousWrappedButton = document.querySelector('.button--previous-wrapped');
+// previousWrappedButton.addEventListener('click', function () {
+//   flkty.previous(true);
+// });
+
+// FUNKCJA DO PASKA PRZEWIJANIA
+
+// var flkty = new Flickity('.carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
+});
