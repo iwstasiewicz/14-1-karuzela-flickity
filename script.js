22fec73d27a516c7b3933777888e71f878231385
Iwona Stasiewicz <iiwka@o2.pl>
2019-08-06 15:18:16 +0200

    add complete zadanie_14.1_karuzela flickity

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-var flkty = new Flickity('.carousel');
+var flkty = new Flickity('.carousel', {
+  "hash": true
+});
 
 // FUNKCJA DO BUTTONA "RESTART"
 
 var previousButton = document.querySelector('.button--previous');
 previousButton.addEventListener('click', function () {
-  flkty.previous();
-});
-
-var previousWrappedButton = document.querySelector('.button--previous-wrapped');
-previousWrappedButton.addEventListener('click', function () {
-  flkty.previous(true);
+  flkty.select(0);
 });
 
 // FUNKCJA DO PASKA PRZEWIJANIA
-
-var flkty = new Flickity('.carousel');
 
 var progressBar = document.querySelector('.progress-bar')
 
