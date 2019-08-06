cb9a4caeadaaa196e2cab45bed32764831e80aec
Iwona Stasiewicz <iiwka@o2.pl>
2019-08-06 18:29:38 +0200

    mustache did not work yet

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ flkty.on('scroll', function (progress) {
   progress = Math.max(0, Math.min(1, progress));
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+(function () {
+
+  var templateList = document.getElementById('template-product-list').innerHTML;
+  var templateItem = document.getElementById('template-product-item').innerHTML;
+
+  Mustache.parse(templateItem);
+
+  var listItems = '';
+
+  for (var i = 0; i < productsData.length; i++) {
+    console.log(productsData);
+    listItems += Mustache.render(templateItem, productsData[i]);
+  }
+
+  var fullProductList = Mustache.render(templateList, {
+    products: listItems
+  });
+
+  results.insertAdjacentHTML('beforeend', fullProductList);
+
+
+})();
